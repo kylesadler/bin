fcbc95603d8a3af22d895037581654da0619c700
Kyle Sadler <kylesadler5@gmail.com>
2022-08-28 20:05:17 -0500

    add new javascript functions

--- util.js
+++ util.js
@@ -1,18 +1,52 @@
-
-const tryAgain = (boolFn, resolve) => {
-  const result = boolFn();
-  if (result) {
+const tryAgain = (loadingFn, resolve) => {
+  const result = loadingFn();
+  if (result != undefined && result != null) {
     resolve(result);
   } else {
     setTimeout(() => {
-      tryAgain(boolFn, resolve);
+      tryAgain(loadingFn, resolve);
     }, 100);
   }
 };
 
-export function load(boolFn) {
+export const load = (loadingFn) => {
+  // loadingFn = () => value. Assumed value is loaded if not undefined or null
   return new Promise((resolve) => {
-    tryAgain(boolFn, resolve);
+    tryAgain(loadingFn, resolve);
   });
-}
+};
 
+export const _try = (fn, defaultValue = undefined) => {
+  try {
+    return fn();
+  } catch {
+    return defaultValue;
+  }
+};
+
+export const localStorageAvailable = (type = "localStorage") => {
+  var storage;
+  try {
+    storage = window[type];
+    var x = "__storage_test__";
+    storage.setItem(x, x);
+    storage.removeItem(x);
+    return true;
+  } catch (e) {
+    return (
+      e instanceof DOMException &&
+      // everything except Firefox
+      (e.code === 22 ||
+        // Firefox
+        e.code === 1014 ||
+        // test name field too, because code might not be present
+        // everything except Firefox
+        e.name === "QuotaExceededError" ||
+        // Firefox
+        e.name === "NS_ERROR_DOM_QUOTA_REACHED") &&
+      // acknowledge QuotaExceededError only if there's something already stored
+      storage &&
+      storage.length !== 0
+    );
+  }
+};
